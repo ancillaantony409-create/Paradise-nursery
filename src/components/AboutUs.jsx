7f3ece1e056910e1769c7d0acc28840f92845b44
ancillaantony409-create <ancillaantony409@gmail.com>
2026-02-22 11:46:49 +0530

    Add AboutUs component with nursery information

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,12 @@
+const AboutUs = () => {
+  return (
+    <div>
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Paradise Nursery provides high-quality plants for your home and garden.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
